6acce62d9f212b5fa3bfae7ba90259b0766156b3
misselvexu <iskp.me@gmail.com>
2020-04-09 15:42:41 +0800

    merge common api module

--- starters/mixmicro-components-starter-message-queue/src/main/java/xyz/vopen/mixmicro/components/boot/mq/autoconfigure/MessageQueueAutoConfiguration.java
+++ starters/mixmicro-components-starter-message-queue/src/main/java/xyz/vopen/mixmicro/components/boot/mq/autoconfigure/MessageQueueAutoConfiguration.java
@@ -1,9 +1,9 @@
 package xyz.vopen.mixmicro.components.boot.mq.autoconfigure;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.config.EnableIntegration;
+import xyz.vopen.mixmicro.components.boot.mq.MessageQueueProperties;
 
 /**
  * {@link MessageQueueAutoConfiguration}
@@ -12,7 +12,7 @@ import org.springframework.integration.config.EnableIntegration;
  * @version ${project.version} - 2020/4/8
  */
 @Configuration
-@EnableConfigurationProperties(ConfigurationProperties.class)
+@EnableConfigurationProperties(MessageQueueProperties.class)
 @EnableIntegration
 public class MessageQueueAutoConfiguration {
 
