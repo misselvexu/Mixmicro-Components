a69a9167b0e7889b66cf40849541b964b5754ca1
misselvexu <iskp.me@gmail.com>
2020-08-26 14:12:03 +0800

    fix sidecar s ip not config issues

--- starters/mixmicro-components-starter-sidecar/src/main/java/xyz/vopen/mixmicro/components/boot/sidecar/SidecarProperties.java
+++ starters/mixmicro-components-starter-sidecar/src/main/java/xyz/vopen/mixmicro/components/boot/sidecar/SidecarProperties.java
@@ -1,25 +1,37 @@
 package xyz.vopen.mixmicro.components.boot.sidecar;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import java.net.URI;
+import java.io.IOException;
+import java.net.*;
+import java.util.Enumeration;
 
 import static xyz.vopen.mixmicro.components.boot.sidecar.SidecarProperties.SIDECAR_CONFIG_PREFIX;
 
 @Validated
 @ConfigurationProperties(prefix = SIDECAR_CONFIG_PREFIX)
-public class SidecarProperties {
+public class SidecarProperties implements InitializingBean {
+
+  private static final Logger log = LoggerFactory.getLogger(SidecarProperties.class);
 
   public static final String SIDECAR_CONFIG_PREFIX = "mixmicro.sidecar";
 
-  /** polyglot service's ip. */
+  /**
+   * polyglot service's ip.
+   */
   private String ip;
 
-  /** polyglot service's port. */
+  /**
+   * polyglot service's port.
+   */
   @NotNull
   @Max(65535)
   @Min(1)
@@ -31,7 +43,9 @@ public class SidecarProperties {
    */
   private URI healthCheckUrl;
 
-  /** interval of health check. */
+  /**
+   * interval of health check.
+   */
   private long healthCheckInterval = 30000L;
 
   public String getIp() {
@@ -64,5 +78,57 @@ public class SidecarProperties {
 
   public void setHealthCheckInterval(long healthCheckInterval) {
     this.healthCheckInterval = healthCheckInterval;
+  }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+
+    if (StringUtils.isBlank(ip)) {
+      this.ip = findFirstNonLoopbackAddress().getHostAddress();
+    }
+
+  }
+
+  public InetAddress findFirstNonLoopbackAddress() {
+    InetAddress result = null;
+    try {
+      int lowest = Integer.MAX_VALUE;
+      for (Enumeration<NetworkInterface> nics = NetworkInterface
+          .getNetworkInterfaces(); nics.hasMoreElements(); ) {
+        NetworkInterface ifc = nics.nextElement();
+        if (ifc.isUp()) {
+          if (ifc.getIndex() < lowest || result == null) {
+            lowest = ifc.getIndex();
+          } else {
+            continue;
+          }
+
+          // @formatter:off
+            for (Enumeration<InetAddress> addrs = ifc
+                .getInetAddresses(); addrs.hasMoreElements();) {
+              InetAddress address = addrs.nextElement();
+              if (address instanceof Inet4Address
+                  && !address.isLoopbackAddress()) {
+                result = address;
+              }
+            }
+          // @formatter:on
+        }
+      }
+    } catch (IOException ex) {
+      log.error("Cannot get first non-loopback address", ex);
+    }
+
+    if (result != null) {
+      return result;
+    }
+
+    try {
+      return InetAddress.getLocalHost();
+    } catch (UnknownHostException e) {
+      log.warn("Unable to retrieve localhost");
+    }
+
+    return null;
   }
 }
