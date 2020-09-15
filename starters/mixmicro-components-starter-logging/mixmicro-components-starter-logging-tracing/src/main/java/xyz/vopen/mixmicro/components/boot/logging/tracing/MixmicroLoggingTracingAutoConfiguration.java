190daca8ac5df0f3dce8d24077309d8ce903d1f0
tino.tang <tino.tang@yunlsp.com>
2020-09-15 11:53:33 +0800

    log client删除对于LoggingFactoryBean的强依赖

--- starters/mixmicro-components-starter-logging/mixmicro-components-starter-logging-tracing/src/main/java/xyz/vopen/mixmicro/components/boot/logging/tracing/MixmicroLoggingTracingAutoConfiguration.java
+++ starters/mixmicro-components-starter-logging/mixmicro-components-starter-logging-tracing/src/main/java/xyz/vopen/mixmicro/components/boot/logging/tracing/MixmicroLoggingTracingAutoConfiguration.java
@@ -28,77 +28,77 @@ import java.util.List;
  * @author <a href="mailto:iskp.me@gmail.com">Elve.Xu</a>
  */
 @Configuration
-@ConditionalOnClass({LoggingFactoryBean.class, EnableLoggingClient.class})
+@ConditionalOnClass(EnableLoggingClient.class)
 @EnableConfigurationProperties(MixmicroLoggingTracingProperties.class)
 @AutoConfigureAfter(WebMvcAutoConfiguration.class)
 @ConditionalOnWebApplication
 @EnableAsync
 @Import({
-    MixmicroLoggingTracingOpenfeignAutoConfiguration.class,
-    MixmicroLoggingTracingRestTemplateAutoConfiguration.class,
-    MixmicroLoggingTracingWebAutoConfiguration.class,
-    MixmicroLoggingTracingGlobalLogStorageAutoConfiguration.class
+        MixmicroLoggingTracingOpenfeignAutoConfiguration.class,
+        MixmicroLoggingTracingRestTemplateAutoConfiguration.class,
+        MixmicroLoggingTracingWebAutoConfiguration.class,
+        MixmicroLoggingTracingGlobalLogStorageAutoConfiguration.class
 })
 public class MixmicroLoggingTracingAutoConfiguration {
-  /**
-   * logger instance
-   */
-  static Logger logger = LoggerFactory.getLogger(MixmicroLoggingTracingAutoConfiguration.class);
-  /**
-   * Mixmicro Boot Logging Properties
-   */
-  private final MixmicroLoggingTracingProperties mixmicroLoggingTracingProperties;
+    /**
+     * logger instance
+     */
+    static Logger logger = LoggerFactory.getLogger(MixmicroLoggingTracingAutoConfiguration.class);
+    /**
+     * Mixmicro Boot Logging Properties
+     */
+    private final MixmicroLoggingTracingProperties mixmicroLoggingTracingProperties;
 
-  public MixmicroLoggingTracingAutoConfiguration(MixmicroLoggingTracingProperties mixmicroLoggingTracingProperties) {
-    this.mixmicroLoggingTracingProperties = mixmicroLoggingTracingProperties;
-  }
-
-  /**
-   * logging factory bean {@link LoggingFactoryBean}
-   *
-   * @param loggingAdminDiscoveryObjectProvider Logging Admin Discovery Instance Provider
-   * @return LoggingFactoryBean
-   */
-  @Bean
-  @ConditionalOnMissingBean
-  public LoggingFactoryBean loggingFactoryBean(
-      ObjectProvider<LoggingAdminDiscovery> loggingAdminDiscoveryObjectProvider,
-      ObjectProvider<List<LoggingFactoryBeanCustomizer>> customizerObjectProvider) {
-    LoggingFactoryBean factoryBean = new LoggingFactoryBean();
-    factoryBean.setIgnorePaths(mixmicroLoggingTracingProperties.getIgnorePaths());
-    factoryBean.setIgnoreHttpStatus(mixmicroLoggingTracingProperties.getIgnoreHttpStatus());
-    factoryBean.setReportAway(mixmicroLoggingTracingProperties.getReportWay());
-    factoryBean.setNumberOfRequestLog(mixmicroLoggingTracingProperties.getReportNumberOfRequestLog());
-    factoryBean.setReportInitialDelaySecond(mixmicroLoggingTracingProperties.getReportInitialDelaySecond());
-    factoryBean.setReportIntervalSecond(mixmicroLoggingTracingProperties.getReportIntervalSecond());
-    factoryBean.setLoggingAdminDiscovery(loggingAdminDiscoveryObjectProvider.getIfAvailable());
-    factoryBean.setShowConsoleLog(mixmicroLoggingTracingProperties.isShowConsoleLog());
-    factoryBean.setFormatConsoleLog(mixmicroLoggingTracingProperties.isFormatConsoleLogJson());
-
-    List<LoggingFactoryBeanCustomizer> customizers = customizerObjectProvider.getIfAvailable();
-    if (!ObjectUtils.isEmpty(customizers)) {
-      customizers.forEach(customizer -> customizer.customize(factoryBean));
+    public MixmicroLoggingTracingAutoConfiguration(MixmicroLoggingTracingProperties mixmicroLoggingTracingProperties) {
+        this.mixmicroLoggingTracingProperties = mixmicroLoggingTracingProperties;
     }
-    logger.info("【LoggingFactoryBean】init successfully.");
-    return factoryBean;
-  }
 
-  /**
-   * Logging Report Scheduled Task Job When the configuration parameter
-   * "mixmicro.boot.logging.report-away=timing" is configured, the creation timing task is performed
-   * to report log information to admin node {@link MixmicroLoggingTracingProperties} {@link
-   * LoggingReportScheduled}
-   *
-   * @param factoryBean logging factory bean
-   * @return LoggingReportScheduled
-   */
-  @Bean
-  @ConditionalOnProperty(
-      prefix = MixmicroLoggingTracingProperties.MIXMICRO_BOOT_LOGGING_PREFIX,
-      name = "report-away",
-      havingValue = "timing")
-  @ConditionalOnMissingBean
-  public LoggingReportScheduled loggingReportScheduled(LoggingFactoryBean factoryBean) {
-    return new LoggingReportScheduled(factoryBean);
-  }
+    /**
+     * logging factory bean {@link LoggingFactoryBean}
+     *
+     * @param loggingAdminDiscoveryObjectProvider Logging Admin Discovery Instance Provider
+     * @return LoggingFactoryBean
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public LoggingFactoryBean loggingFactoryBean(
+            ObjectProvider<LoggingAdminDiscovery> loggingAdminDiscoveryObjectProvider,
+            ObjectProvider<List<LoggingFactoryBeanCustomizer>> customizerObjectProvider) {
+        LoggingFactoryBean factoryBean = new LoggingFactoryBean();
+        factoryBean.setIgnorePaths(mixmicroLoggingTracingProperties.getIgnorePaths());
+        factoryBean.setIgnoreHttpStatus(mixmicroLoggingTracingProperties.getIgnoreHttpStatus());
+        factoryBean.setReportAway(mixmicroLoggingTracingProperties.getReportWay());
+        factoryBean.setNumberOfRequestLog(mixmicroLoggingTracingProperties.getReportNumberOfRequestLog());
+        factoryBean.setReportInitialDelaySecond(mixmicroLoggingTracingProperties.getReportInitialDelaySecond());
+        factoryBean.setReportIntervalSecond(mixmicroLoggingTracingProperties.getReportIntervalSecond());
+        factoryBean.setLoggingAdminDiscovery(loggingAdminDiscoveryObjectProvider.getIfAvailable());
+        factoryBean.setShowConsoleLog(mixmicroLoggingTracingProperties.isShowConsoleLog());
+        factoryBean.setFormatConsoleLog(mixmicroLoggingTracingProperties.isFormatConsoleLogJson());
+
+        List<LoggingFactoryBeanCustomizer> customizers = customizerObjectProvider.getIfAvailable();
+        if (!ObjectUtils.isEmpty(customizers)) {
+            customizers.forEach(customizer -> customizer.customize(factoryBean));
+        }
+        logger.info("【LoggingFactoryBean】init successfully.");
+        return factoryBean;
+    }
+
+    /**
+     * Logging Report Scheduled Task Job When the configuration parameter
+     * "mixmicro.boot.logging.report-away=timing" is configured, the creation timing task is performed
+     * to report log information to admin node {@link MixmicroLoggingTracingProperties} {@link
+     * LoggingReportScheduled}
+     *
+     * @param factoryBean logging factory bean
+     * @return LoggingReportScheduled
+     */
+    @Bean
+    @ConditionalOnProperty(
+            prefix = MixmicroLoggingTracingProperties.MIXMICRO_BOOT_LOGGING_PREFIX,
+            name = "report-away",
+            havingValue = "timing")
+    @ConditionalOnMissingBean
+    public LoggingReportScheduled loggingReportScheduled(LoggingFactoryBean factoryBean) {
+        return new LoggingReportScheduled(factoryBean);
+    }
 }
